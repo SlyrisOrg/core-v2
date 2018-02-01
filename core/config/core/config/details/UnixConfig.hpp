c63fca42a6958417bebbf377d93fb15552b0a9be
Doom <clement.doumergue@epitech.eu>
2018-02-01 09:14:14 +0100

    add the Config module
    Change-Id: I83ef43cec994d1eef32f3a6eb570782d7ec269dc

---
+++ core/config/core/config/details/UnixConfig.hpp
@@ -0,0 +1,10 @@
+//
+// Created by szterg_r on 22/08/2017.
+//
+
+#ifndef CORE_UTILS_UNIXCONFIG_HPP
+#define CORE_UTILS_UNIXCONFIG_HPP
+
+#define USING_UNIX
+
+#endif //CORE_UTILS_UNIXCONFIG_HPP
---
+++ core/config/core/config/details/WindowsConfig.hpp
@@ -0,0 +1,10 @@
+//
+// Created by szterg_r on 22/08/2017.
+//
+
+#ifndef CORE_UTILS_WINDOWSCONFIG_HPP
+#define CORE_UTILS_WINDOWSCONFIG_HPP
+
+#define USING_WINDOWS
+
+#endif //CORE_UTILS_WINDOWSCONFIG_HPP
