6c7f13a302c8fa00351d48373dca758f0db7047f
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-02-01 08:57:54 +0100

    add jenkins scripts
    Change-Id: Icd21cf01066e5e4b5db6bb2048e07e629e584ce1

---
+++ scripts/jenkins-build.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+PROGNAME=$0
+
+CCOMPILER="gcc"
+CXXCOMPILER="g++"
+TYPE="Debug"
+
+function parse_args()
+{
+    while getopts ":c:x:t:" opt; do
+	case $opt in
+	    c)
+		CXXCOMPILER=$(which $OPTARG)
+		;;
+	    x)
+		CCOMPILER=$(which $OPTARG)
+		;;
+	    t)
+		TYPE=$OPTARG
+		;;
+	    h)
+		echo "Usage: $PROGNAME [-c <compiler>] [-t <Debug | Release>]"
+		exit 0
+		;;
+	    \?)
+		echo "Invalid argument -$OPTARG, see $PROGNAME -h"
+		;;
+	esac
+    done
+
+    shift $(expr $OPTIND - 1)
+    for t in "$@"; do
+	TARGETS+="$t "
+    done
+}
+
+function print_settings()
+{
+    echo "C compiler: $CCOMPILER"
+    echo "C++ compiler: $CXXCOMPILER"
+    echo "Build type: $TYPE"
+}
+
+function build_targets
+{
+    mkdir -p $BUILD_DIR
+    cd $BUILD_DIR
+    cmake -DCMAKE_BUILD_TYPE=$TYPE -DCMAKE_C_COMPILER=$CCOMPILER -DCMAKE_CXX_COMPILER=$CXXCOMPILER -DCORE_BUILD_TESTS=ON ..
+    make -j2
+    cd ..
+}
+
+function run_ctest
+{
+    cd $BUILD_DIR
+    ctest --no-compress-output -T Test -D ExperimentalMemCheck
+    cd ..
+}
+
+function run_xunit
+{
+    cd bin
+    for i in *-test; do
+	./$i --gtest_output="xml:${i}-${TYPE}-result.xml"
+    done
+    cd ..
+    mkdir -p test-result/ctest
+    cp bin/*.xml test-result/
+    cp $BUILD_DIR/Testing/*/*.xml test-result/ctest/
+}
+
+parse_args "$@"
+BUILD_DIR=${CCOMPILER}-${TYPE}-build
+print_settings
+build_targets
+run_ctest
+run_xunit
