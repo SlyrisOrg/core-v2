42be12eb81d53ea279c4d3404100d59ee71f071f
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-02-14 08:55:46 +0100

    add range algorithm erase_if
    Change-Id: Ibd0d780956d96ede97a665595ccd74af337b4451

---
+++ tests/containers-test/range-test.cpp
@@ -0,0 +1,67 @@
+//
+// Created by milerius on 14/02/18.
+//
+
+#include <gtest/gtest.h>
+#include <core/containers/range/erase_if.hpp>
+
+class TestingVectorFixture : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        static_assert(!ctr::range::is_associative_container_v<VecInt>, "");
+    }
+
+public:
+    using VecInt = std::vector<int>;
+    VecInt v{1, 2, 32, 17, 81, 27, 7, 9};
+};
+
+class TestingMapFixture : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        static_assert(ctr::range::is_associative_container_v<MapInt>, "");
+    }
+
+public:
+    using MapInt = std::map<int, int>;
+    MapInt map{
+        {1, 2},
+        {2, 13},
+        {3, 37},
+        {4, 8},
+        {5, 13}
+    };
+};
+
+TEST_F(TestingVectorFixture, erase_if)
+{
+    auto copy = v;
+    auto functor = [](int nb) { return nb > 10; };
+    ctr::range::erase_if(v, functor);
+    copy.erase(std::remove_if(begin(copy), end(copy), functor));
+    ASSERT_EQ(copy, v);
+}
+
+TEST_F(TestingMapFixture, erase_if)
+{
+    auto copy = map;
+    auto functor = [](int nb) { return nb > 10; };
+
+    auto erase_if_functor = [&func = functor, &container = copy]() {
+        for (auto it = std::begin(container); it != std::end(container);) {
+            if (func(it->second)) {
+                it = container.erase(it);
+            } else {
+                ++it;
+            }
+        }
+    };
+
+    ctr::range::erase_if(map, functor);
+    erase_if_functor();
+    ASSERT_EQ(map.size(), copy.size());
+}
