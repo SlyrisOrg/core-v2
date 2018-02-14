d25349e3a54c32ecd2eb4f5e5381521ddcdbd80a
Roman Sztergbaum <roman.sztergbaum@epitech.eu>
2018-02-14 08:55:46 +0100

    add range algorithm erase_if
    Change-Id: Ibd0d780956d96ede97a665595ccd74af337b4451

--- core/containers/CMakeSources.cmake
+++ core/containers/CMakeSources.cmake
@@ -4,6 +4,7 @@ set(MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/core/containers)
 
 set(MODULE_PUBLIC_HEADERS
         ${MODULE_PATH}/Span.hpp
+        ${MODULE_PATH}/range/erase_if.hpp
         )
 
 set(MODULE_PRIVATE_HEADERS "")
